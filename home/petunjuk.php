52c9ad7297c7a698232a9daee137fffc78e0f22f
Iqbal Revvin <48580953+iqbalrevvin@users.noreply.github.com>
2019-09-28 22:30:14 +0700

    Update petunjuk.php

--- home/petunjuk.php
+++ home/petunjuk.php
@@ -3,7 +3,7 @@
                     <div class="card-content">
                     
                       <div id="table-datatables">
-                        <h4 class="header">Contoh Video Perawatan AC</h4>
+                        <h4 class="header">Petunjuk Pengobatan</h4>
                         <div class="row">
                           <div class="col s12 m12 20">
                          
@@ -12,12 +12,26 @@
                                 <source src="mov_bbb.ogg" type="video/ogg">
                                 Your browser does not support HTML5 video.
                               </video> -->
-                              <ul>
-                                <li>- Cara menggunakan obat homeopathy</li>
-                                <li>- Jika pasien menggunakan</li>
-                                <li>- Tips pencegahan</li>
+                              <ul class="collapsible">
+                                <li>
+                                  <div class="collapsible-header"><i class="mdi-action-info"></i>Cara menggunakan obat homeopathy</div>
+                                  <div class="collapsible-body">
+                                    <p>Konten Deskripsi (Edit di home/petunjuk.php)</p><!-- ISI KONTEN DI DALAM TAG <P> BOSS-->
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="collapsible-header"><i class="mdi-action-info"></i>Jika pasien menggunakan</div>
+                                  <div class="collapsible-body">
+                                    <p>Konten Deskripsi (Edit di home/petunjuk.php)</p><!-- ISI KONTEN DI DALAM TAG <P> BOSS-->
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="collapsible-header"><i class="mdi-action-info"></i>Tips pencegahan</div>
+                                  <div class="collapsible-body">
+                                    <p>Konten Deskripsi (Edit di home/petunjuk.php)</p><!-- ISI KONTEN DI DALAM TAG <P> BOSS-->
+                                  </div>
+                                </li>
                               </ul>
-                              
                             <!-- <span>Sumber Video : Youtube</span><br>
                             <span>Link Video : <a href="https://www.youtube.com/watch?v=zJAsz9FV6eY&feature=youtu.be">https://www.youtube.com/watch?v=zJAsz9FV6eY&feature=youtu.be</a></span> -->
                           </div>
